f90a736f331eb0ce485f125d2caf8a42dd2d9bbe
Yan Krstic <yankrstic@gmail.com>
2024-04-02 04:16:18 -0300

    criando as tarefas

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function t(){
-    let a = "oi"
-    let t = document.getElementById("teste2")
-    t.innerHTML = a
+//let div = document.getElementById('.modelo-tarefa')
+//div.style.display = 'none'
+
+function NovaTarefa(){
+    const modeloTarefa = document.querySelector('.modelo-tarefa')
+    const novaTarefa = modeloTarefa.cloneNode(true)
+
+    let nome = document.getElementById('ipt-nome')
+    let desc = document.getElementById('ipt-descricao')
+
+    let nomeV = nome.value
+    let descV = desc.value
+    
+    if(nomeV.replace(/\s/g,'') == "" || descV.replace(/\s/g,'') == "" ){
+        alert("Preencha as informações")
+    }
+    else{
+
+    nome.value = ''
+    desc.value = ''
+
+    const nomeTarefa = novaTarefa.querySelector('.nome-tarefa')
+    nomeTarefa.textContent = nomeV
+    novaTarefa.style.display = 'block'
+    
+    const listaTarefas = document.getElementById('lista-tarefas')
+    listaTarefas.appendChild(novaTarefa)
+
+
+    novaTarefa.classList.remove('modelo-tarefa')
+    }
+
 }
 
